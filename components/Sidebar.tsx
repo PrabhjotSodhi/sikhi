a40c2e1c0abc895ed42f1090b6070c3815695cf0
Prabhjot Sodhi <sodhiprabhjot23@gmail.com>
2023-12-17 04:45:01 +0000

    Sidebar Library Inital Styling

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,37 +1,43 @@
 "use client";
 
-import { HiHome } from "react-icons/hi";
-import { BiSearch } from "react-icons/bi";
-import { usePathname } from "next/navigation";
-import { useMemo } from "react";
+import { TbPlaylist } from "react-icons/tb";
+import { AiOutlinePlus } from "react-icons/ai";
+
+import Box from "./Box";
 
 interface SidebarProps {
     children: React.ReactNode;
 }
 
 const Sidebar = ({ children }: SidebarProps) => {
-    const pathname = usePathname();
+  const onClick = () => {
+  }
 
-    const routes = useMemo(() => [
-        {
-        icon: HiHome,
-        label: 'Home',
-        active: pathname !== '/search',
-        href: '/'
-        },
-        {
-        icon: BiSearch,
-        label: 'Search',
-        href: '/search',
-        active: pathname === '/search'
-        },
-    ], [pathname]);
-
-    return (
-        <div>
-            [children]
-        </div>
-    )
+  return (
+    <div className="flex h-full">
+      <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+        <Box className="overflow-y-auto h-full">
+          <div className="flex flex-col">
+            <div className="flex items-center justify-between px-5 pt-4">
+              <div className="inline-flex items-center gap-x-2">
+                <TbPlaylist className="text-neutral-400" size={26} />
+                <p className="text-neutral-400 font-medium text-md">
+                  Your Library
+                </p>
+              </div>
+              <AiOutlinePlus onClick={onClick} size={20} className="text-neutral-400 cursor-pointer hover:text-white transition" />
+            </div>
+            <div className="flex flex-col gap-y-2 mt-4 px-3">
+              Random Shabad Here!
+            </div>
+          </div>
+        </Box>
+      </div>
+      <main className="h-full flex-1 overflow-y-auto py-2">
+        {children}
+      </main>
+    </div>
+  )
 } 
 
 export default Sidebar;
