f2c4bbef35dbd387ccaa323ddd8b6ea23737c24b
Prabhjot Sodhi <sodhiprabhjot23@gmail.com>
2023-12-18 01:51:42 +0000

    Button Component

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { useRouter } from "next/navigation";
 import { FaUserAlt } from "react-icons/fa";
 import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 
 interface HeaderProps {
@@ -22,7 +23,7 @@ const Header: React.FC<HeaderProps> = ({
     <div className={twMerge("h-fit bg-gradient-to-b from-amber-800 p-6", className)}>
       <div className="w-full mb-4 flex items-center justify-between">
         <div className="hidden md:flex gap-x-2 items-center">
-          <button className="rounded-full bg-black flex items-center justify-center cursor-pointer hover:opacity-75 transition">
+          <button onClick={() => router.back()} className="rounded-full bg-black flex items-center justify-center cursor-pointer hover:opacity-75 transition">
             <RxCaretLeft className="text-white" size={35} />
           </button>
           <button onClick={() => router.forward()} className="rounded-full bg-black flex items-center justify-center cursor-pointer hover:opacity-75 transition">
@@ -34,9 +35,18 @@ const Header: React.FC<HeaderProps> = ({
         </div>
         <div className="flex justify-between items-center gap-x-4">
         </div>
+        <div className="flex justify-between items-center gap-x-4">
+            <>
+              <div>
+                <Button className="bg-transparent text-neutral-300 font-medium">Sign up</Button>
+              </div>
+              <div>
+                <Button className="bg-white px-6 py-2">Log in</Button>
+              </div>
+            </>
+        </div>
       </div>
-      {children}
-    </div>
+      </div>
   );
 }
 
