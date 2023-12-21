7117f1eca567f44a5f67116bbca1a11d8ff47267
Prabhjot Sodhi <sodhiprabhjot23@gmail.com>
2023-12-21 03:42:22 +0000

    Supabase Auth Modal integration

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -1,11 +1,23 @@
 "use client";
 
+import { useSessionContext, useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useRouter } from "next/navigation";
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+
 import Modal from "./Modal"
 
+
+
 const AuthModal = () => {
+    const supabaseClient = useSupabaseClient();
+    const router = useRouter();
+    const { session } = useSessionContext();
+
+
     return (
         <Modal title="Welcome Back" description="Sign in to your account" isOpen onChange={() => {}}>
-            Auth Modal Children!
+            <Auth theme="dark" magicLink providers={["google"]} supabaseClient={supabaseClient} appearance={{theme: ThemeSupa, variables: {default: {colors: {brand: "#404040", brandAccent: "#004080"}}} }} />
         </Modal>
     )
 };
