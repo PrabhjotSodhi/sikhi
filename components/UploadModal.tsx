7f40259244ac924ecdc7998b861be16a7d194386
Prabhjot Sodhi <sodhiprabhjot23@gmail.com>
2023-12-22 04:09:16 +0000

    Input Tailwind styling

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -1,21 +1,41 @@
 "use client"
 
 import useUploadModal from "@/hooks/useUploadModal";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { useState } from "react";
+
 import Modal from "./Modal"
+import Input from "./Input";
 
 const UploadModal = () => {
+    const [Loading, isLoading] = useState();
     const uploadModal = useUploadModal();
+
+    const { register, handleSubmit, reset } = useForm<FieldValues>({
+        defaultValues: {
+            author: "",
+            title: "",
+            shabad: null,
+            image: null,
+        }
+    })
 
     const onChange = (open: boolean) => {
         if (!open) {
-            // Reset the form
+            reset();
             uploadModal.onClose();
         }
     }
 
+    const onSubmit: SubmitHandler<FieldValues> = async (values) => {
+        // Upload Shabad to Supabase
+    }
+
     return (
-        <Modal title="Upload Modal Title" description="Upload Modal Description" isOpen={uploadModal.isOpen} onChange={onChange}>
-            Upload Content
+        <Modal title="Add a Shabad" description="Upload an mp3 file" isOpen={uploadModal.isOpen} onChange={onChange}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Input id="title" disabled={isLoading} {...register('title', { required: true })} placeholder="Song Title" />
+            </form>
         </Modal>
     )
 }
