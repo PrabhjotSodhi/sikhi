624c5bdeafd7136bea109280a9b47e87649d4b2e
Prabhjot Sodhi <sodhiprabhjot23@gmail.com>
2023-12-22 23:32:27 +0000

    Shabad type setup and getShabads action

---
+++ actions/getShabads.ts
@@ -0,0 +1,16 @@
+import { Shabad } from "@/types";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+
+const getShabads = async (): Promise<Shabad[]> => {
+    const supabase = createServerComponentClient({cookies: cookies});
+    
+    const { data, error } = await supabase.from('shabads').select('*').order('created_at', { ascending: false });
+    if (error) {
+        console.log(error);
+    }
+
+    return (data as any) || [];
+}
+
+export default getShabads;
--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,22 +1,24 @@
+import getShabads from "@/actions/getShabads";
 import Header from "@/components/Header";
 
-export default function Home() {
-  return (
-    
+export const revalidate = 0;
+
+export default async function Home() {
+  const shabads = await getShabads();
+
+  return (    
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header>
         <div className="mb-2">
           <h1 className="text-white text-3xl font-semibold">Welcome back</h1>
         </div>
-        <div className="mt-2 mb-7 px-6">
-          <div className="flex justify-between items-center">
-            <h2 className="text-white text-2xl font-semibold">Recently Played</h2>
-          </div>
-          <div className="flex justify-between items-center">
-            <h2 className="text-white text-2xl font-semibold">Newest songs</h2>
-          </div>
-        </div>
       </Header>
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h2 className="text-white text-2xl font-semibold">Newest Shabads</h2>
+        </div>
+        <PageContent />
+      </div>
     </div>
   )
 }
