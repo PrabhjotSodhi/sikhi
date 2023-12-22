fc98c81af5b550295ee590546bf64d29e119f53c
Prabhjot Sodhi <sodhiprabhjot23@gmail.com>
2023-12-22 06:29:59 +0000

    Upload Modal inputs

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -6,9 +6,11 @@ import { useState } from "react";
 
 import Modal from "./Modal"
 import Input from "./Input";
+import Button from "./Button";
 
 const UploadModal = () => {
-    const [Loading, isLoading] = useState();
+    const [isLoading, setIsLoading] = useState(false);
+
     const uploadModal = useUploadModal();
 
     const { register, handleSubmit, reset } = useForm<FieldValues>({
@@ -18,7 +20,7 @@ const UploadModal = () => {
             shabad: null,
             image: null,
         }
-    })
+    });
 
     const onChange = (open: boolean) => {
         if (!open) {
@@ -33,8 +35,22 @@ const UploadModal = () => {
 
     return (
         <Modal title="Add a Shabad" description="Upload an mp3 file" isOpen={uploadModal.isOpen} onChange={onChange}>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <Input id="title" disabled={isLoading} {...register('title', { required: true })} placeholder="Song Title" />
+            <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-4">
+                <Input id="title" disabled={isLoading} {...register('title', { required: true })} placeholder="Shabad Name" />
+                <Input id="author" disabled={isLoading} {...register('author', { required: true })} placeholder="Author Name" />
+                <div>
+                    <div className="pb-1">
+                        Select a Shabad file
+                    </div>
+                    <Input id="shabad" type="file" disabled={isLoading} accept=".mp3" {...register('shabad', { required: true })} />
+                </div>
+                <div>
+                    <div className="pb-1">
+                        Select a Thumbnail Image
+                    </div>
+                    <Input id="image" type="file" disabled={isLoading} accept="image/*" {...register('image', { required: true })} />
+                </div>
+                <Button disabled={isLoading} type="submit">Import Shabad</Button>
             </form>
         </Modal>
     )
